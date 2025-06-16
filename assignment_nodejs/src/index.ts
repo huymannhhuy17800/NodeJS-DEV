d7a2e997d9cc76dd74683d0099ef996cf57e27d5
NguyenManhHuy <68103157+huymannhhuy17800@users.noreply.github.com>
2025-06-16 20:38:18 +0700

    add https server

--- assignment_nodejs/src/index.ts
+++ assignment_nodejs/src/index.ts
@@ -3,15 +3,22 @@ import router from "./routes/user.route";
 import path from "path";
 import productRouter from "./routes/product.route";
 import { apiKeyMiddleware } from "./middleware/product.auth";
-import multer from "multer";
-import bodyParser from "body-parser";
 import uploadRouter from "./routes/upload.route";
+import fs from "fs";
+import https from "https";
 
 // const express = require("express");
 const app = express();
 const port = 3000;
 
+const httpsPort = 3001;
+
 const baseUrl = "/api";
+
+const options = {
+  key: fs.readFileSync(path.resolve("cert/key.pem")),
+  cert: fs.readFileSync(path.resolve("cert/cert.pem")),
+};
 
 // const publicDir = path.resolve("src/page");
 // const uploadsDir = path.resolve("src/uploads");
@@ -41,3 +48,13 @@ app.use(baseUrl, apiKeyMiddleware, productRouter);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//Create folder cert and run script to get 2 SSL certificate files 'key.pem' and 'cert.pem'
+https
+  .createServer(options, (req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Welcome to HTTPS Server</h1>");
+  })
+  .listen(httpsPort, () => {
+    console.log(`Example app listening on port ${httpsPort}`);
+  });
